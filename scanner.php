def6187226327ca4a40d727c651c4ba1c2cdb735
nephelim74 <117781748+nephelim74@users.noreply.github.com>
2024-03-30 16:09:48 +0300

    Update scanner.php - add var timeout

--- scanner.php
+++ scanner.php
@@ -3,10 +3,11 @@
 $ip="localhost";
 $start_port = 1;
 $end_port = 1024;
+$timeout = 0.01;
 print("Начинаем сканирование");
 // Loop through the port range and check each one
 for ($port = $start_port; $port <= $end_port; $port++) {
- $connection = @fsockopen($ip, $port, $errno, $errstr, 1);
+ $connection = @fsockopen($ip, $port, $errno, $errstr, $timeout);
  if (is_resource($connection)) {
  echo "Port $port is open\n";
  fclose($connection);
