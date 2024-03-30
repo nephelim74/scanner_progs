29ca9f9cae6bc2801314768c683ec1d9d743d3b2
nephelim74 <nephelim@ya.ru>
2024-03-30 13:42:14 +0300

    edit file scanner.php on main

--- scanner.php
+++ scanner.php
@@ -3,6 +3,7 @@
 $ip="localhost";
 $start_port = 1;
 $end_port = 1024;
+print("Начинаем сканирование");
 // Loop through the port range and check each one
 for ($port = $start_port; $port <= $end_port; $port++) {
  $connection = @fsockopen($ip, $port, $errno, $errstr, 1);
